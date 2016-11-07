e9f700f6ccc539e24c2a49478d78ccc78822ef6e
Roman Mylenkyi <i.ua4@i.ua>
2016-11-07 17:24:14 +0000

    3

--- js/index.js
+++ js/index.js
@@ -2,9 +2,9 @@
 //console.log('hallow2');
 //document.write('hallow3');
 
-var a = Number(prompt('Fonmula quadratic equation: A*х^2 + B*x + C = 0; input argument A'));
-var b = Number(prompt('Fonmula quadratic equation: A*х^2 + B*x + C = 0; input argument B'));
-var c = Number(prompt('Fonmula quadratic equation: A*х^2 + B*x + C = 0; input argument C'));
+var a = Number(prompt('The formula of quadratic equation is: A*X^2 + B*X + C = 0. Input argument A'));
+var b = Number(prompt('The formula of quadratic equation is: A*X^2 + B*X + C = 0. Input argument B'));
+var c = Number(prompt('The formula of quadratic equation is: A*X^2 + B*X + C = 0. Input argument C'));
 
 var d = b*b - 4*a*c;
 var result0;
@@ -12,13 +12,15 @@ var result1;
 var result2;
 var resultplus1;
 var resultplus2;
+var conditions = 'The solution of quadratic equation: A*X^2 + B*X + C = 0, <br \/> <br \/> provided that A=' + a + '; B=' + b + '; C=' + c + ':<br \/> <br \/>';
 
-resultplus1 = 'x1 = ' + (-b + Math.sqrt(d)) / (2 * a);
-resultplus2 = 'x2 = ' + (-b - Math.sqrt(d)) / (2 * a);
-var conditions = 'The solution of quadratic equation: A*х^2 + B*x + C = 0, <br \/> <br \/> provided that A=' + a + '; B=' + b + '; C=' + c + ':<br \/> <br \/>';
-var result0 = conditions + 'Equation has no solutions';
-var result1 = conditions + 'Discriminant is zero, so the equation has only one solution: ' + resultplus1;
-var result2 = conditions + resultplus1 + '; ' +resultplus2;
+
+resultplus1 = 'X1 = ' + (-b + Math.sqrt(d)) / (2 * a);
+resultplus2 = 'X2 = ' + (-b - Math.sqrt(d)) / (2 * a);
+
+result0 = conditions + 'Discriminant is less than zero, so the equation has no solutions';
+result1 = conditions + 'Discriminant is zero, so the equation has only one solution: <br \/> <br \/>' + resultplus1;
+result2 = conditions + resultplus1 + '; <br \/> <br \/>' +resultplus2;
 
 function result (d) {
     if (d < 0) {
